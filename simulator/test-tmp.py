c595d230667871adb95eba00e20a11939020df46
Misikir <misikir.gebrehiwot@aalto.fi>
2017-11-08 10:00:12 +0200

    removed slurm files

--- simulator/test-tmp.py
+++
@@ -1,6 +0,0 @@
-import fileinput
-
-for line in fileinput.input('inputs/setup_delay_1.0'):
-    print(fileinput.filelineno(), type(fileinput.filelineno()))
-    if str(fileinput.filelineno()) == '3':
-        print(line)

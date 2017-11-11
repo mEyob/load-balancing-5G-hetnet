9468679ec3e27902cf20802038646a72bb87d7a3
Misikir <mikireyob@gmail.com>
2017-11-11 12:29:53 +0200

    adding gitignore

--- simulator/param_values.py
+++ simulator/param_values.py
@@ -11,19 +11,11 @@ setup_delay = args.s
 
 arg_params = []
 
-<<<<<<< HEAD
-for macro_arrival_rate in [2]:
-    for smll_arrival_rate in [1, 4, 6, 9]:
-        for idle in [0, 1, 2, 4, 8, 128, 512, 1024]:
-            arg_params.append([macro_arrival_rate, smll_arrival_rate, setup_delay, idle/smll_arrival_rate])
-=======
 with open('inputs/setup_delay_'+str(setup_delay), 'w') as f:
     for laMac in [2]:
         for laS in [1, 4, 6, 9]:
             for idle in [0, 0.125, 0.25, 0.5,  1, 2, 4, 8, 128, 512, 1024]:
                 f.write(' '.join(list(map(str, [laMac, laS, setup_delay, idle/laS])))+'\n')
->>>>>>> 0485bbe6144c81d804028ec9cbf978d732764e65
-
 
 
 
